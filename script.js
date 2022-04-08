a82d216a8ec25da3adead226c32b46956247a1b9
Tej Patel <tejpatel317@gmail.com>
2022-04-08 09:07:15 -0400

    simple interest calc, html and java

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+let button = document.getElementById("simplebutton")
 
-document.querySelector("body").appendChild(h2);
+function simpleInterestCalculator() {
+    let userPrinciple = parseFloat(document.getElementById("simplePrincipal").value, 10);
+    console.log(typeof userPrinciple);
+    let userInterest = parseFloat(document.getElementById("simpleInterest").value, 10);
+    let userTerm = parseFloat(document.getElementById("simpleTerm").value, 10);
+    let userSimpleInterest = (userPrinciple * userInterest * userTerm)/100;
+    let userSimpleTotal = userPrinciple + userSimpleInterest;
+    console.log(userSimpleInterest, userPrinciple, userInterest, userTerm);
+    headingSimpleResult = document.getElementById("simpleResult");
+    headingSimpleResult.innerHTML = `Your total amount is ${userSimpleTotal.toFixed(2)}$ with an interest of ${userSimpleInterest.toFixed(2)}$`;
+}
+
+button.addEventListener("click", function (e) {
+    simpleInterestCalculator();
+});
